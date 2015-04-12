6d9bcd93eb13bfe9f3804d3348b27a17f27ddaed
Fuyuan Cheng <gloomcheng@gmail.com>
2015-04-12 14:52:42 +0800

    Extra GRANT permission I needed.

--- container/init.sh
+++ container/init.sh
@@ -28,8 +28,9 @@ if [ ! -d /var/lib/mysql/mysql ]; then
   mysql -uroot -e "UPDATE mysql.user set Password=PASSWORD('$PW') where user = 'root';"
   mysql -uroot -e "CREATE DATABASE $DB CHARACTER SET utf8 COLLATE utf8_general_ci;"
   mysql -uroot -e "CREATE USER '$DB'@'%' IDENTIFIED BY '$PW';"
+  mysql -uroot -e "GRANT SELECT, LOCK TABLES ON 'mysql'.* TO '$DB'@'%';"
   mysql -uroot -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, LOCK TABLES, TRIGGER, \
-      CREATE TEMPORARY TABLES ON $DB.* TO '$DB'@'%';"
+      SHOW VIEW, EVENT, CREATE TEMPORARY TABLES ON $DB.* TO '$DB'@'%';"
   mysql -uroot -e "FLUSH PRIVILEGES;"
   echo "MySQL initialize completed !!"
   echo "MYSQL_DB=$DB"
