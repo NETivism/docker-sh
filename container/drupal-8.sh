c0f75ff713945a28de3cf5bbb693fa930cdfd52c
Gloom Cheng <gloomcheng@gmail.com>
2016-05-31 23:37:35 +0800

    Add drupal-8.sh to build drupal site. refs #17748.

---
+++ container/drupal-8.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+date +"@ %Y-%m-%d %H:%M:%S %z"
+
+# wait for mysql start
+while ! pgrep -u mysql mysqld > /dev/null; do sleep 3; done
+sleep 10
+
+DB=$INIT_DB
+PW=$INIT_PASSWD
+DOMAIN=$INIT_DOMAIN
+BASE="/var/www"
+DRUPAL="8.1.1"
+SITE=$INIT_NAME
+MAIL=$INIT_MAIL
+HOST_MAIL=$HOST_MAIL
+
+# init script repository
+cd /home/docker && git pull
+
+# init log directory
+if [ ! -d /var/www/html/log ]; then
+  mkdir /var/www/html/log
+  chown root /var/www/html/log
+fi
+chgrp -R www-data $BASE/html/log && chmod -R g+ws $BASE/html/log
+
+# init mysql
+DB_EXISTS=`mysql -uroot -sN -e "SHOW databases" | grep $DB`
+
+if [ -z "$DB_EXISTS" ] && [ -n "$DB" ]; then
+  mysql -uroot -e "CREATE DATABASE $DB CHARACTER SET utf8 COLLATE utf8_general_ci;"
+  mysql -uroot -e "CREATE USER '$DB'@'%' IDENTIFIED BY '$PW';"
+  mysql -uroot -e "GRANT ALL PRIVILEGES ON $DB.* TO '$DB'@'%' WITH GRANT OPTION;"
+  mysql -uroot -e "UPDATE mysql.user set Password=PASSWORD('$PW') where user = 'root';"
+  mysql -uroot -e "FLUSH PRIVILEGES;"
+  echo "MySQL initialize completed !!"
+  echo "MYSQL_DB=$DB"
+  echo "MYSQL_PW=$PW"
+
+  if [ -f "$BASE/html/index.php" ]; then
+    DRUPAL_EXISTS=`cat $BASE/html/index.php | grep drupal`
+  else
+    DRUPAL_EXISTS=""
+  fi
+  if [ -z "$DRUPAL_EXISTS" ]; then
+    date +"@ %Y-%m-%d %H:%M:%S %z"
+    echo "Install Drupal ..."
+    cd $BASE
+    drush dl drupal-${DRUPAL}
+    mv $BASE/drupal-${DRUPAL}/* $BASE/html/
+    mv $BASE/drupal-${DRUPAL}/.htaccess $BASE/html/
+    rm -Rf $BASE/drupal-${DRUPAL}
+  fi
+
+  # make sure drush have correct base_url
+  if [ ! -f "$BASE/html/sites/default/drushrc.php" ]; then
+    echo -e "<?php\n\$options['uri'] = 'http://$INIT_DOMAIN';\n\$options['php-notices'] = 'warning';" > $BASE/html/sites/default/drushrc.php;
+  fi
+
+  if [ -f $BASE/html/sites/default/settings.php ]; then
+    echo "Error: Drupal already installed. (found settings.php)"
+    exit 1
+  fi
+
+  cd $BASE/html
+  php ~/.composer/vendor/bin/drush.php site-install standard --account-mail="${HOST_MAIL}" --account-name=admin --account-pass=${PW} --db-url=mysql://${DB}:${PW}@127.0.0.1/${DB} --site-mail=${MAIL} --site-name="${SITE}" --locale=zh-hant --yes
+
+  cd $BASE && chown -R www-data:www-data html
+  echo "Done!"
+else
+  echo "Skip exist $DB, root password already setup before."
+fi
+date +"@ %Y-%m-%d %H:%M:%S %z"
+
