7381330f790f84fdee1ce25d726b51296d5607df
Jimmy Huang <jimmy@netivism.com.tw>
2016-08-02 21:26:59 +0800

    fixes windows mysql file permission problem.

--- boot2docker.sh
+++ boot2docker.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "Usage: ./$0 domain.com mail@mail.com neticrm-7.sh"
+
 REALPATH=`realpath $0`
 WORKDIR=`dirname $REALPATH`
 MOUNTDIR=$WORKDIR
@@ -23,6 +24,15 @@ if [ ! -d "$MOUNTDIR/mysql/sites/$1" ]; then
   mkdir -p $MOUNTDIR/mysql/sites/$1
 fi
 
+# pickup port
+for WWWPORT in $(seq 30000 1 31000); do
+  RESULT=`nc -z 127.0.0.1 "$WWWPORT" && echo 1`
+  if [ -z "$RESULT" ]; then
+    break
+  fi
+done
+DBPORT=`expr $WWWPORT + 1`
+
 cd $MOUNTDIR/neticrm-7
 git clone -b develop https://github.com/NETivism/netiCRM.git civicrm
 cd civicrm
@@ -37,15 +47,15 @@ else
   SCRIPT="$WORKDIR/container/neticrm-7.sh"
 fi
 
-docker run -d --name $1 \
-  -p 8888:80 \
-  -p 33333:3306 \
+if [ -n "$WWWPORT" ] && [ -n "$DBPORT" ]; then
+  docker run -d --name $1 \
+  -p $WWWPORT:80 \
+  -p $DBPORT:3306 \
   -v $MOUNTDIR/www/sites/$1:/var/www/html \
   -v $MOUNTDIR/mysql/sites/$1:/var/lib/mysql \
   -v /etc/localtime:/etc/localtime:ro \
   -v $SCRIPT:/init.sh \
   -v $MOUNTDIR/neticrm-7:/mnt/neticrm-7 \
-  -v $WORKDIR/mysql/my.cnf:/etc/mysql/my.cnf \
   -e INIT_DB=develop \
   -e INIT_PASSWD=123456 \
   -e INIT_DOMAIN=$1 \
@@ -55,3 +65,8 @@ docker run -d --name $1 \
   -e "TZ=Asia/Taipei" \
   -w "/var/www/html" \
   -i -t netivism/docker-wheezy-php55
+
+  docker cp $WORKDIR/mysql/my.cnf /etc/mysql/my.cnf
+  docker ps -f "name=$1"
+  echo "$1 is listen on http://127.0.0.1:$WWWPORT"
+fi
