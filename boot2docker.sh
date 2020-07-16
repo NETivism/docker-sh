b367fc3ede83862cae6b89deb0f6f5a27dd008f4
Jimmy Huang <jimmy@netivism.com.tw>
2020-07-16 18:07:53 +0800

    refs #28750, correct exists container detection

--- boot2docker.sh
+++ boot2docker.sh
@@ -75,9 +75,9 @@ if [ "$REBUILD" -eq "1" ]; then
     exit 1
   fi
 else
-  QUIET=`docker ps -f "name=$1"`
+  QUIET=`docker ps -q -f "name=$1" | wc -l`
   RESULT=$?
-  if [ $RESULT -eq 0 ]; then
+  if [ $RESULT -eq 1 ]; then
     echo -e "Error: exists container $1 detected. Add --rebuild parameter and try again"
     echo -e "This commend will update repository to latest verion"
     echo -e "This will not touch your civicrm / neticrm / neticrmp project code"
