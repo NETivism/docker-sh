1d1fd44d94ae9f6196a42be3d820d5f859c5b5ad
Jimmy Huang <jimmy@netivism.com.tw>
2024-12-07 22:06:15 +0800

    Remove pecl download progress

--- container/peopo-10.sh
+++ container/peopo-10.sh
@@ -19,8 +19,11 @@ fi
 #42010, install ffmpeg
 if [ $(dpkg -l | grep "^ii.*ffmpeg" | wc -l) -eq 0 ]; then
   apt-get update > /dev/null 2>&1
-  DEBIAN_FRONTEND=noninteractive apt-get install -y ffmpeg php8.2-uploadprogress
-  DEBIAN_FRONTEND=noninteractive apt-get remove -y apache2 apache2-bin apache2-data apache2-utils file libapache2-mod-php libapache2-mod-php8.3 libapr1 libaprutil1 libaprutil1-dbd-sqlite3 libaprutil1-ldap libjansson4 liblua5.3-0 libmagic-mgc libmagic1 mailcap mime-support php8.3-cli php8.3-common php8.3-opcache php8.3-readline php8.4-cli php8.4-common php8.4-opcache php8.4-phpdbg php8.4-readline php8.4-uploadprogress
+  DEBIAN_FRONTEND=noninteractive apt-get install -y ffmpeg
+  # use jquery upload progress instead
+  # https://www.drupal.org/project/drupal/issues/2833968
+  # DEBIAN_FRONTEND=noninteractive apt-get install -y ffmpeg php8.2-uploadprogress
+  # DEBIAN_FRONTEND=noninteractive apt-get remove -y apache2 apache2-bin apache2-data apache2-utils file libapache2-mod-php libapache2-mod-php8.3 libapr1 libaprutil1 libaprutil1-dbd-sqlite3 libaprutil1-ldap libjansson4 liblua5.3-0 libmagic-mgc libmagic1 mailcap mime-support php8.3-cli php8.3-common php8.3-opcache php8.3-readline php8.4-cli php8.4-common php8.4-opcache php8.4-phpdbg php8.4-readline php8.4-uploadprogress
 fi
 
 date +"@ %Y-%m-%d %H:%M:%S %z"
