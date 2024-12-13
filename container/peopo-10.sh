a08fb2fd34baf03c8400d25ca50b6fa2e8953dab
Jimmy Huang <jimmy@netivism.com.tw>
2024-12-13 08:53:58 +0800

    Add high availability start up vps adjust

--- container/peopo-10.sh
+++ container/peopo-10.sh
@@ -6,24 +6,33 @@ if [ ! -d /var/www/html/log ]; then
   mkdir /var/www/html/log
   chown root /var/www/html/log
 fi
-if [ -f /var/www/html/log/php.ini ]; then
-  for DIR in /etc/php/* ; do
-    if [ -d "$DIR/fpm/conf.d" ]; then
-      cd "$DIR/fpm/conf.d" && ln -s /var/www/html/log/php.ini xx-php.ini
-      supervisorctl restart php-fpm
-    fi
-  done
-  sleep 3
-fi
+
+# install drush
+composer require drush/drush
 
 #42010, install ffmpeg
 if [ $(dpkg -l | grep "^ii.*ffmpeg" | wc -l) -eq 0 ]; then
   apt-get update > /dev/null 2>&1
   DEBIAN_FRONTEND=noninteractive apt-get install -y ffmpeg
-  # use jquery upload progress instead
-  # https://www.drupal.org/project/drupal/issues/2833968
-  # DEBIAN_FRONTEND=noninteractive apt-get install -y ffmpeg php8.2-uploadprogress
-  # DEBIAN_FRONTEND=noninteractive apt-get remove -y apache2 apache2-bin apache2-data apache2-utils file libapache2-mod-php libapache2-mod-php8.3 libapr1 libaprutil1 libaprutil1-dbd-sqlite3 libaprutil1-ldap libjansson4 liblua5.3-0 libmagic-mgc libmagic1 mailcap mime-support php8.3-cli php8.3-common php8.3-opcache php8.3-readline php8.4-cli php8.4-common php8.4-opcache php8.4-phpdbg php8.4-readline php8.4-uploadprogress
 fi
+
+#upload video support
+if [[ "$TranscoderID" == load* ]]; then
+  cat > /etc/php/8.2/fpm/conf.d/x_peopo_custom.ini << EOF
+upload_max_filesize = 2G
+post_max_size = 2G
+memory_limit = 512M
+opcache.memory_consumption = 512
+opcache.jit_buffer_size = 256M
+EOF
+fi
+
+#allowed more www user visit
+if [[ "$TranscoderID" == www* ]]; then
+  sed -i 's/pm\.max_children = 8/pm.max_children = 16/g' /etc/php/8.2/fpm/pool.d/www.conf
+fi
+
+sleep 3
+supervisorctl restart php-fpm
 
 date +"@ %Y-%m-%d %H:%M:%S %z"
